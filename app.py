771c3662932a958d273d79867e4c1265f4bf9f67
Dorian Barboza <dorianbarboza@gmail.com>
2021-04-23 20:27:32 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://dorian:pass12345@localhost/unitarias'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10))
+    password = db.Column(db.String(10))
+
+    def serialize(self):
+        return {
+                'id', self.id,
+                'username', self.username,
+                'password', self.password
+                }
+
+@app.route('/get_users')
+def get_all_users():
+    users = User.query.all()
+    return jsonify({
+    [user.serialize() for user in users]
+    }), 200
+
+@app.route('/post_users', methods=['POST'])
+def post_user():
+    username = request.form.get['username']
+    password = request.form.get['password']
+
+    new_user=User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    db.create_all()
